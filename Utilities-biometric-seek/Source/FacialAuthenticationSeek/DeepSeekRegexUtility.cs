59932ed65d8380d49dce3e664a94973ebe289734
Abner Rodas <54729077+SrPalermo@users.noreply.github.com>
2025-03-03 07:58:23 -0600

    Delete Utilities-biometric-seek directory

--- Utilities-biometric-seek/Source/FacialAuthenticationSeek/DeepSeekRegexUtility.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.RegularExpressions;
-
-namespace utilities_biometric_seek.Source.FacialAuthenticationSeek;
-
-public static partial class DeepSeekRegexUtility
-{
-    [GeneratedRegex(@"```json|\```")]
-    public static partial Regex JsonRegex();
-}
