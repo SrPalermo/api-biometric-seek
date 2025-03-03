26463b64968a560b538d8063fa7f4e811953f8d3
Abner Rodas <54729077+SrPalermo@users.noreply.github.com>
2025-03-03 07:51:03 -0600

    Delete models directory

--- models/Common/Parameters/FacialAuthenticationSeekParameters.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace models_biometric_seek.Common.Parameters;
-
-public enum FacialAuthenticationSeekParameters
-{
-    IMG_BASE = 1,
-    IMG_REF = 2,
-    UNKNOWN = 0
-}
--- models/Sources/FacialAuthenticationSeek/Internal/Request/FacialAuthenticationSeekRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace models_biometric_seek.Sources.FacialAuthenticationSeek.Internal.Request;
-
-public class FacialAuthenticationSeekRequest
-{
-    public IFormFile ImageBase { get; set; } = null!;
-    public IFormFile ImageReference { get; set; } = null!;
-}
